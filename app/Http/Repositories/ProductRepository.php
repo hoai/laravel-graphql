9fdce68defe5837661993c1ffee2545bf6c37dfa
wakka.van.hoai <van_hoai@lampart-vn.com>
2020-10-21 14:12:35 +0700

    fix db raw

--- app/Http/Repositories/ProductRepository.php
+++ app/Http/Repositories/ProductRepository.php
@@ -28,26 +28,33 @@ class ProductRepository
         $users =  Product::with($with)->select($fields->getSelect())->paginate($args['limit'], ['*'], 'page', $args['page']);
         echo  json_encode($users); exit;*/
 
-        $with = array_keys($fields->getRelations());
+        //echo  json_encode($fields->getRelations()); exit;
 
+        $with = array_keys($fields->getRelations());
+        $with_columns = [];
         $products_columns  = DB::select( DB::raw("SELECT GROUP_CONCAT(CONCAT(' ', table_name,'.', column_name, ' as `', table_name, '.', column_name, '`')) as name FROM information_schema.columns WHERE table_name = :table_variable"), array(
             'table_variable' => 'products'
         ));
-        $products_images_columns  = DB::select( DB::raw("SELECT GROUP_CONCAT(CONCAT(' ', table_name,'.', column_name, ' as `', table_name, '.', column_name, '`')) as name FROM information_schema.columns WHERE table_name = :table_variable"), array(
-            'table_variable' => 'product_images'
-        ));
-        $users_columns  = DB::select( DB::raw("SELECT GROUP_CONCAT(CONCAT(' ', table_name,'.', column_name, ' as `', table_name, '.', column_name, '`')) as name FROM information_schema.columns WHERE table_name = :table_variable"), array(
-            'table_variable' => 'users'
-        ));
-       //print_r($products_columns);exit;
+        $with_columns['products'] = $products_columns[0]->name;
+        foreach($with as $key => $val){
 
-        echo  json_encode($products_columns[0]->name); exit;
-        $users = DB::table('products')
-            ->join('product_images', 'products.id', '=', 'product_images.product_id')
-            ->join('users', 'products.user_id', '=', 'users.id')
-            ->where('products.id' , $args['id'])->select(DB::raw( $products_columns[0]->name ))->paginate();
+            $rr  = DB::select( DB::raw("SELECT GROUP_CONCAT(CONCAT(' ', table_name,'.', column_name, ' as `', table_name, '.', column_name, '`')) as name FROM information_schema.columns WHERE table_name = :table_variable"), array(
+                'table_variable' => $val
+            ));
+            $with_columns[$val] = $rr[0]->name;
+        }
 
-       echo  json_encode($users); exit;
+        //echo  json_encode($products_columns[0]->name); exit;
+        $users = DB::table('products');
+        if(in_array('product_images', $with)){
+            $users->join('product_images', 'products.id', '=', 'product_images.product_id');
+        }
+        if(in_array('users', $with)){
+            $users->join('users', 'products.user_id', '=', 'users.id');
+        }
+        $mm = $users->where('products.id' , $args['id'])->select(DB::raw( implode(",", $with_columns) ))->paginate();
+
+       echo  json_encode($mm); exit;
         return $users;
 
 
